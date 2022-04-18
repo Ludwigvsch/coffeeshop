a206acc7f3252039455f2958a5e3ac58d57be328
Ludwigvsch <lvschoenfeldt@gmx.de>
2022-04-17 19:27:01 -0700

    changing buttons on home

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -68,10 +68,7 @@ class Header extends Component {
             <Fade bottom duration={2000}>
               <ul className="social">
                 <a href={project} className="button btn project-btn">
-                  <i className="fa fa-book"></i>Project
-                </a>
-                <a href={github} className="button btn github-btn">
-                  <i className="fa fa-github"></i>Github
+                  <i className="fa fa-book"></i>Menu
                 </a>
               </ul>
             </Fade>
