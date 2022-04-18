0f7c902935ddbbbb75105303cff33b9ac4ec44d9
Ludwigvsch <lvschoenfeldt@gmx.de>
2022-04-17 19:21:43 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import $ from "jquery";
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Resume from "./Components/Products";
+import Contact from "./Components/Contact";
+import Portfolio from "./Components/Portfolio";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      foo: "bar",
+      Data: {}
+    };
+  }
+
+  Data() {
+    $.ajax({
+      url: "./Data.json",
+      dataType: "json",
+      cache: false,
+      success: function(data) {
+        this.setState({ Data: data });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log(err);
+        alert(err);
+      }
+    });
+  }
+
+  componentDidMount() {
+    this.Data();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header data={this.state.Data.main} />
+        <About data={this.state.Data.main} />
+        <Resume data={this.state.Data.resume} />
+        <Portfolio data={this.state.Data.portfolio} />
+        <Contact data={this.state.Data.main} />
+        <Footer data={this.state.Data.main} />
+      </div>
+    );
+  }
 }
 
 export default App;
